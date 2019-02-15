40d5c57f9cc1fda723e208447d91821110aec2e2
Ben Smus <bensmus@gmail.com>
2019-02-15 15:46:23 -0800

    It did not work, will test a completely new approach, involving all-cardinal-ones

---
+++ all-cardinal-ones.py
@@ -0,0 +1,21 @@
+import numpy as np
+import helper as h
+
+matrix = np.array([
+[1, 1, 0, 0],
+[0, 0, 1, 1],
+[1, 1, 0, 0],
+])
+
+# full test
+ONES_COORS = h.ones(matrix)
+print(ONES_COORS)
+
+while True: # loop to find the cardinal ones of coordinate
+    #print(np.array(matrix2))
+    print(matrix)
+    xcoor = int(input("x coor? "))
+    ycoor = int(input("y coor? "))
+    cardinal_ones = h.cardinalOnes(ONES_COORS, [xcoor, ycoor])
+    print(cardinal_ones)
+    print()
