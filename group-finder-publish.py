8c9bbc299b23552641f6078b3532c4ff8c0417aa
Unknown <bensmus@gmail.com>
2019-02-16 12:13:37 -0800

    Added comments
    General info, steps, function descriptions

--- group-finder-publish.py
+++ group-finder-publish.py
@@ -1,8 +1,18 @@
 import numpy as np
 import random as rm
 
+'''this program is based on trying to find clusters of connected
+stones in the game "go".
+Stones of a certain color are modelled as ones
+in a 2d matrix and everything else as zeroes'''
+
+'''Program generates a random matrix of ones and zeroes and then
+changes the first "1" it finds (scanning from top left) and all
+of the "1"'s connected to it (according to the rules of go) into
+"8"'s'''
 
 def passed(matrix, coordinate):
+    '''function changes matrix element into an 8'''
     matrix[coordinate[1]][coordinate[0]] = 8
     return matrix
 
@@ -18,6 +28,7 @@ def ones(matrix):
 
 
 def cardinal(coordinate):
+    '''used to find "cardinal ones"'''
     cardinal_coors = []
     cardinal_coors.append([coordinate[0] + 1, coordinate[1]])
     cardinal_coors.append([coordinate[0] - 1, coordinate[1]])
@@ -27,6 +38,7 @@ def cardinal(coordinate):
 
 
 def matrixAnd(matrix1, matrix2):
+    '''used to find "cardinal ones" '''
     bothMatrix = []
     for row1 in matrix1:
         for row2 in matrix2:
@@ -36,12 +48,18 @@ def matrixAnd(matrix1, matrix2):
 
 
 def cardinalOnes(ONES_COORS, coordinate):
+    '''cardinal ones are a set of "1" elements surrounding
+    a certain coordinate (adjacent to the coordinate
+    by one step in the cardinal direction) '''
+
     cardinal_coors = cardinal(coordinate)
     cardinal_ones = matrixAnd(cardinal_coors, ONES_COORS)
     return cardinal_ones
 
 
 def getGroup(ONES_COORS, coor):
+    '''a group is formed of cardinal ones and
+    their source coordinate'''
     cardinal_ones = cardinalOnes(ONES_COORS, coor)
     group = [i for i in cardinal_ones]
     group.append(coor)
@@ -58,15 +76,18 @@ def belongsTest(group, test_group):
 
 
 def _3d_2d(_3d):
+    '''turn a 3d array into a 2d array
+    join cardinal groups together'''
     _2d = []
-    d1 = len(_3d)
-    d2 = len(_3d[0])  # unused data
-    for i in range(d1):
-        # some 2d matrices are longer than others...
+    d = len(_3d)
+    for i in range(d):
+        # some 2d matrices (cardinal groups) are larger than others...
         for j in range(len(_3d[i])):
             _2d.append(_3d[i][j])
     return _2d
 
+
+# 1: Make a random matrix containing zeroes and ones
 
 matrix = np.array([
     [rm.randint(0, 1) for i in range(10)] for i in range(10)
@@ -74,6 +95,9 @@ matrix = np.array([
 
 print(matrix)
 print()
+
+# 2: Get all of the cardinal groups (sets of 2, 3 or 4 ones)
+# that are in horizontal or vertical adjacency
 
 ONES_COORS = ones(matrix)
 groups = []
@@ -86,6 +110,8 @@ for coor in ONES_COORS:
 shape = [groups[0]]
 groups.pop(0)
 
+# 3: Look at which of these cardinal groups have a common element
+
 # we will be adding group to "shape", and removing group from "groups"
 for group in shape:
     for test_group in groups:
@@ -94,6 +120,9 @@ for group in shape:
             groups.remove(test_group)
 
 shape = _3d_2d(shape)
+
+# 4: Represent the first combination of cardinal groups as
+# eights on the matrix
 
 # remove duplicates in the 2d array
 shape = [list(t) for t in set(tuple(element) for element in shape)]
