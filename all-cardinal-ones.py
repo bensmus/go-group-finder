533b6601f041a752aeeaf127750ef4fa02b9a425
Ben Smus <bensmus@gmail.com>
2019-02-15 17:09:49 -0800

    Getting closer!

--- all-cardinal-ones.py
+++ all-cardinal-ones.py
@@ -1,12 +1,13 @@
 import numpy as np
 import helper as h
 
-'''matrix = np.array([
-[1, 1, 0, 0],
-[0, 0, 1, 1],
-[1, 1, 0, 0],
-])
+matrix = np.array([
+    [1, 1, 0, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 0],
+]); print(matrix)
 
+'''
 # full POC
 ONES_COORS = h.ones(matrix)
 print(ONES_COORS)
@@ -20,18 +21,61 @@ while True: # loop to find the cardinal ones of coordinate
     print(cardinal_ones)
     print()'''
 
-matrix = np.array([
-[1, 1, 0, 0],
-[0, 1, 1, 1]
-]); print(matrix)
-
+'''print(matrix)
 ONES_COORS = h.ones(matrix)
 
-for coor in ONES_COORS:
-    print(coor, h.cardinalOnes(ONES_COORS, coor))
+for i, coor in enumerate(ONES_COORS):
+    print(i, ":", coor, h.cardinalOnes(ONES_COORS, coor))'''
 
-    # in the output, do you notice a diagonal relationship?
-    # it continues fully!
+# in the output, do you notice a diagonal relationship?
+# it continues fully!
 
-    # by changing the element at coordinate [1, 1] from a
-    # one to a zero, we can see this relationship break apart
+# by changing the element at coordinate [1, 1] from a
+# one to a zero, we can see this relationship break apart
+
+''' terminal output:
+[[1 1 0 1]
+ [1 0 1 1]
+ [1 1 0 0]]
+0 : [0, 0] [[1, 0], [0, 1]]
+1 : [1, 0] [[0, 0]]             1 and 0
+2 : [3, 0] [[3, 1]]
+3 : [0, 1] [[0, 2], [0, 0]]     3 and 1 and 0
+4 : [2, 1] [[3, 1]]
+5 : [3, 1] [[2, 1], [3, 0]]
+6 : [0, 2] [[1, 2], [0, 1]]     6 and 3 and 1 and 0
+7 : [1, 2] [[0, 2]]             7 and 6 and 3 and 1 and 0
+'''
+
+# therefore: group is [1, 2], [0, 2], [0, 1], [0, 0]
+# it's the manhunt algorithm!
+
+ONES_COORS = h.ones(matrix)
+coor = ONES_COORS[0]
+
+
+def belongsCombine(row1, row2):
+    for a in row1:
+        for b in row2:
+            if a == b:
+                return row1 + row2
+
+
+cardinal_ones = h.cardinalOnes(ONES_COORS, coor)
+group = [i for i in cardinal_ones]
+group.append(coor)
+print(group)
+
+# original group created #
+# this should be a function #
+
+def getGroup(ONES_COORS, coor):
+    cardinal_ones = h.cardinalOnes(ONES_COORS, coor)
+    group = [i for i in cardinal_ones]
+    group.append(coor)
+    return group
+
+print(getGroup(ONES_COORS, [2, 0]))
+'''
+[[3, 0], [1, 0], [2, 1], [2, 0]]
+'''
