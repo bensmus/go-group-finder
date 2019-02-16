1c3f06c8be4ce93c2dce6f12016a9584d4009b04
Ben Smus <bensmus@gmail.com>
2019-02-15 16:06:53 -0800

    the diagonal relationship for finding groups

--- all-cardinal-ones.py
+++ all-cardinal-ones.py
@@ -1,13 +1,13 @@
 import numpy as np
 import helper as h
 
-matrix = np.array([
+'''matrix = np.array([
 [1, 1, 0, 0],
 [0, 0, 1, 1],
 [1, 1, 0, 0],
 ])
 
-# full test
+# full POC
 ONES_COORS = h.ones(matrix)
 print(ONES_COORS)
 
@@ -18,4 +18,20 @@ while True: # loop to find the cardinal ones of coordinate
     ycoor = int(input("y coor? "))
     cardinal_ones = h.cardinalOnes(ONES_COORS, [xcoor, ycoor])
     print(cardinal_ones)
-    print()
+    print()'''
+
+matrix = np.array([
+[1, 1, 0, 0],
+[0, 1, 1, 1]
+]); print(matrix)
+
+ONES_COORS = h.ones(matrix)
+
+for coor in ONES_COORS:
+    print(coor, h.cardinalOnes(ONES_COORS, coor))
+
+    # in the output, do you notice a diagonal relationship?
+    # it continues fully!
+
+    # by changing the element at coordinate [1, 1] from a
+    # one to a zero, we can see this relationship break apart
